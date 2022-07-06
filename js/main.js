820bc258e51142050de43df273b955eea0745f42
octavio vercellobe <90575217+OctavioVer@users.noreply.github.com>
2022-07-06 01:47:07 -0300

    Segunda entrega lista

--- js/main.js
+++ js/main.js
@@ -1,54 +1,76 @@
-
 alert("Hola bienvenido/a A nuestra calculadora de pagos en cuotas");
 let monto = Number(prompt("Ingrese un monto deseado: "));
 
+const cuotasInteres = (cant) => {
+  let montoPorCuota;
+  if (cant <= 12) {
+    monto = monto + (monto * 15) / 100;
+    montoPorCuota = monto / cant;
+    alert(`El monto por cuota es de: ${montoPorCuota}`);
+  } else {
+    alert("La cantidad de sus cuotas supera nuestro límite");
+  }
+};
 
-const cuotasInteres =(cant) =>{
-   let montoPorCuota;
-   if(cant <= 12){
-      monto = monto + monto * 15 / 100;
-      montoPorCuota = monto / cant;
-      alert(`El monto por cuota es de: ${montoPorCuota}`);
-   }else{
-      alert("La cantidad de sus cuotas supera nuestro límite");
-   }
-}
-
-const cuotasFijas =(cant) =>{
-   if (cant == 1) {
-      monto = monto;
-      alert(`El monto por cuota es de: ${monto}`)
-   }else if 
-      (cant <= 12) {
-         monto = monto / cant;
-         montoPorCuota = monto;
-         alert(`El monto por cuota es de: ${montoPorCuota}`);
-   }else{
-      alert("La cantidad de sus cuotas supera nuestro límite");
-   }
-}
+const cuotasFijas = (cant) => {
+  if (cant == 1) {
+    monto = monto;
+    alert(`El monto por cuota es de: ${monto}`);
+  } else if (cant <= 12) {
+    monto = monto / cant;
+    montoPorCuota = monto;
+    alert(`El monto por cuota es de: ${montoPorCuota}`);
+  } else {
+    alert("La cantidad de sus cuotas supera nuestro límite");
+  }
+};
 
 do {
-   opcion = Number(prompt("Elija una opción:\n\n1.Pagar en cuotas sin interés\n2.Pagar en cuotas con interes(15%)\n3.Salir"))
-   let cant;
+  opcion = Number(
+    prompt(
+      "Elija una opción:\n\n1.Pagar en cuotas sin interés\n2.Pagar en cuotas con interes(15%)\n3.Salir"
+    )
+  );
+  let cant;
 
-   switch (opcion) {
-      case 1:
-         cant = Number(prompt("Solo se puede pagar en 1 o 12 cuotas, sin interés\nIngrese cuantas cuotas desea: "));
-         cuotasFijas(cant);
-         break;
-      case 2:
-         cant = Number(prompt("Solo se puede pagar en 1 o 12 cuotas, con interés\nIngrese cuantas cuotas desea: "));
-         cuotasInteres(cant);
-         break;
-      case 3:
-         alert("Muchas gracias...");
-         break;
-      default:
-         alert("Opcion incorrecta");
-         break;
-   }
-   
+  switch (opcion) {
+    case 1:
+      cant = Number(
+        prompt(
+          "Solo se puede pagar en 1 o 12 cuotas, sin interés\nIngrese cuantas cuotas desea: "
+        )
+      );
+      cuotasFijas(cant);
+      break;
+    case 2:
+      cant = Number(
+        prompt(
+          "Solo se puede pagar en 1 o 12 cuotas, con interés\nIngrese cuantas cuotas desea: "
+        )
+      );
+      cuotasInteres(cant);
+      break;
+    case 3:
+      alert("Muchas gracias...");
+      break;
+    default:
+      alert("Opcion incorrecta");
+      break;
+  }
 } while (opcion != 3);
 
-alert("Muchas gracias por utilizar nuestro simulador")
+alert("Muchas gracias por utilizar nuestro simulador");
+
+function Persona(nombre, apellido, edad) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.edad = edad;
+}
+
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+let edad = Number(prompt("Ingrese su edad"));
+
+const persona1 = new Persona(nombre, apellido, edad);
+
+console.log(persona1.nombre);
